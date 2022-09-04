84799f02585c9d1fa80b59c1f8b6a72a8f2082fc
AzerDime <guyian@pdx.edu>
2022-09-03 23:37:54 -0700

    hello world but in browser this time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 // Ian Guy, 2022. Working along with a textbook!
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||  {
+        App::new()
+        .route("/", web::get().to(greet))
+        .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
