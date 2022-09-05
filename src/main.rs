15be3a3a611be18c8a666081091d1e0621907fd2
AzerDime <guyian@pdx.edu>
2022-09-05 00:12:54 -0700

    no longer using fixed port

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,11 @@
 //         .run()
 //         .await
 // }
+use std::net::TcpListener;
 use zero2prod::run;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
