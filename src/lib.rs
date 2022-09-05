7581c990a3cc297888247bd7dec0ce65bcf8297c
AzerDime <guyian@pdx.edu>
2022-09-04 23:17:29 -0700

    addition

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// Ian Guy, 2022. What was in main is now in lib! Allows for better black box tests.
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 // Ian Guy, 2022. Working along with a textbook!
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-// Greet is now deprecated.
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+// use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+// // Greet is now deprecated.
+// // async fn greet(req: HttpRequest) -> impl Responder {
+// //     let name = req.match_info().get("name").unwrap_or("World");
+// //     format!("Hello {}!", &name)
+// // }
+
+// async fn health_check(req: HttpRequest) -> impl Responder {
+//     HttpResponse::Ok()
+// }
+
+// #[tokio::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+//         .bind("127.0.0.1:8000")?
+//         .run()
+//         .await
+// }
+
+
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
 }
