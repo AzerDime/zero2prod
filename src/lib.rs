2e5577b56c363c1756ac44671d4194cc9dbc5161
AzerDime <guyian@pdx.edu>
2022-09-06 03:43:27 -0700

    fixed migration failing

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,16 @@ async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
 #[derive(serde::Deserialize)]
 struct FormData {
     email: String,
-    name: String
+    name: String,
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check))
-        .route("/subscriptions", web::post().to(subscribe)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
