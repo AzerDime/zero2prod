a397437d9baf64b9b616e6015b88f08263c1c3ec
AzerDime <guyian@pdx.edu>
2022-09-06 18:36:10 -0700

    head hurts

--- src/startup.rs
+++ src/startup.rs
@@ -5,13 +5,13 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
-
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
